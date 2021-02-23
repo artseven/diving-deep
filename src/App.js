3131a3a4a6e38fe11371220e3ab8453d43d863de
artseven <yabkevich.artsiom.09@gmail.com>
2021-02-23 04:08:13 -0500

    inline styling

--- src/App.js
+++ src/App.js
@@ -37,11 +37,21 @@ const app = (props) => {
     });
   };
 
+  const style = {
+    backgroundColor: "white",
+    font: "inherit",
+    border: "1px solid blue",
+    padding: "8px",
+    cursor: 'pointer'
+  };
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={() => switchNameHandler("Maximilian!!")}>
+      <button 
+        style={style}
+        onClick={() => switchNameHandler("Maximilian!!")}>
         Switch Name
       </button>
       <Person
