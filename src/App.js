89a0b5023850e2e2d4a286673eb40324c4455138
artseven <yabkevich.artsiom.09@gmail.com>
2021-02-26 04:26:58 -0500

    tagged template styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import "./App.css";
 import Person from "./Person/Person";
+
+const StyledButton = styled.button`
+  backgroundColor: ${props => props.alt ? 'red' : 'green'},
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover: {
+    background-color: lightgreen;
+    color: black;
+  },
+`;
 
 const app = (props) => {
   const [personsState, setPersonsState] = useState({
@@ -52,18 +67,7 @@ const app = (props) => {
     });
   };
 
-  const style = {
-    backgroundColor: "green",
-    color: "white",
-    font: "inherit",
-    border: "1px solid blue",
-    padding: "8px",
-    cursor: "pointer",
-    ":hover": {
-      backgroundColor: "lightgreen",
-      color: "black",
-    },
-  };
+  const style = {};
 
   let persons = null;
 
@@ -101,9 +105,12 @@ const app = (props) => {
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p className={classes.join(" ")}>This is really working!</p>
-      <button style={style} onClick={togglePersonsHandler}>
+      <StyledButton
+        alt={personsState.showPersons}
+        onClick={togglePersonsHandler}
+      >
         Toggle Persons
-      </button>
+      </StyledButton>
       {persons}
     </div>
   );
