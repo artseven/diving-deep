adba1d8f7544f3c029ad11fe3290015fab2390f1
artseven <yabkevich.artsiom.09@gmail.com>
2021-03-08 11:14:41 -0500

    performance optimization to prevent re-rendering

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -15,7 +15,11 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[Persons.js] should component update');
-    return true;
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
